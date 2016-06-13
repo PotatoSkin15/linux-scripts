caedd389096a684dc61303fe8176cbddf9c7c707
Kenny <kpeck@gs-mesapocket4.ndc.nasa.gov>
2016-06-13 10:41:30 -0400

    Removed web stack install from flightcheck.sh and expanded it in hydration.sh

--- flightcheck.sh
+++ flightcheck.sh
@@ -26,41 +26,15 @@ else
 
 		# More basics that should be installed for ease of use
 		yum -y install git vim htop wget openssh net-tools epel-release firewalld
-		
+
 		# Webtatic
 		wget https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 		rpm --import https://mirror.webtatic.com/yum/RPM-GPG-KEY-webtatic-el7
 		rpm -K webtatic-release.rpm
 		rpm -Uvh webtatic-release.rpm
 		yum update
-	} > ~/flightcheck_log
-		# Prompt if LAMP stack needed
-
-		echo 'Is a LAMP stack required? [y/n]'
-		read stack
-	
-		if [ "$stack" == "y" ]; then
-	{		# Basics for Linux servers, LAMP stack
-			yum -y install httpd httpd-tools mariadb mariadb-server php php-common php-gd php-xmlrpc php-xml expect openssl openssl-devel
-			
-			# Starts Apache2 and MariaDB/MySQL with Systemd or init script
-
-			if [ "$SYS" == "systemd" ]; then
-				systemctl start httpd && systemctl enable httpd
-				systemctl start mariadb && systemctl enable mariadb
-			elif [ "$SYS" == "sysvinit" ]; then
-				service httpd start && chkconfig httpd on
-				service mariadb start && chkconfig mariadb on
-			fi
-	} >> ~/flightcheck_log		
-		else
-			echo 'Moving on'
-		fi
-	{
-		# Install development tools
-		yum -y groupinstall "Development tools"
 	} >> ~/flightcheck_log
-	
+
 	echo 'Done. Check flightcheck_log for more details'
 
 	elif [ "$OS" == "ubuntu" ]; then
@@ -70,35 +44,13 @@ else
 
 		# Turn off SELinux for now
 		setenforce 0
-		
+
 		# Install basics for ease of use
 		apt-get -y install git vim htop wget openssh net-tools
 
 		# Shut off SELinux if not already
 		sed -i -e 's/enforcing/permissive/g' /etc/selinux/config
 
-		# Prompt if LAMP stack is required
-	} > ~/flightcheck_log
-		echo 'Is a LAMP stack required? [y/n]'
-		read stack
-	{
-		if [ "$stack" == "y" ]; then
-			# Basics for Linux servers, LAMP stack
-			apt-get -y install apache2 apache2-utils mysql-server php5 php5-common php5-gd php5-xmlrpc php5-xml expect openssl openssl-devel
-				
-			# Starts Apache2 and MariaDB/MySQL with Systemd or init script
-			if [ "$SYS" == "systemd" ]; then
-				systemctl start apache2 && systemctl enable apache2
-				systemctl start mysql && systemctl enable mysql
-			elif [ "$SYS" == "sysvinit" ]; then
-				service apache2 start && chkconfig apache2 on
-				service mysql start && chkconfig mysql on
-			fi
-	} >> ~/flightcheck_log
-		else
-			echo 'Moving on'
-		fi
-	{
 		# Install development tools
 		apt-get -y install build-essentials
 
---
+++ hydration.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+clear
+
+# Check distribution before installing packages
+OS=`grep -Eiom 1 'CentOS|RedHat|Ubuntu' /proc/version`
+
+# Check for Systemd vs sysvinit
+SYS=`ps -p 1 -o cmd h`
+
+if [ "$USER" != "root" ]; then
+		echo 'WARNING! This script should be run as root'
+		echo 'Please enter sudo su and run the script again'
+else
+  echo "Make sure you run flightcheck.sh first"
+  sleep 5
+  cat << EOF
+  Select your action:
+  1) Install web server (Apache, nginx)
+  2) Install DB server (MySQL/MariaDB, PGSQL)
+  3) Install GitLab
+  EOF
+
+  read task
+
+  case $task in
+    1)
+      echo "Apache or nginx? [a/n]"
+      read webserver
+
+      if [ "$webserver" == "a" ]; then
+        if [ "$OS" == "centos" -a "redhat" ]; then
+          { # Installs base Apache stack
+          yum -y install httpd httpd-tools php php-common php-gd php-xmlrpc php-xml openssl openssl-devel
+            # Starts Apache2 with Systemd or init script
+            if [ "$SYS" == "systemd" ]; then
+              systemctl start httpd && systemctl enable httpd
+            elif [ "$SYS" == "sysvinit" ]; then
+              service httpd start && chkconfig httpd on
+            fi
+          fi } >> ~/hydration_log
+          echo "Apache successfully installed with OpenSSL and PHP"
+          echo "Check hydration_log for more details"
+        elif [ "$OS" == "ubuntu" ]; then
+          { # Installs base Apache stack
+            apt-get -y install apache2 apache2-utils php5 php5-common php5-gd php5-xmlrpc php5-xml openssl openssl-devel
+            # Starts Apache2 with Systemd or init script
+            if [ "$SYS" == "systemd" ]; then
+              systemctl start apache2 && systemctl enable apache2
+            elif [ "$SYS" == "sysvinit" ]; then
+              service apache2 start && chkconfig apache2 on
+            fi
+          fi } >> ~/hydration_log
+          echo "Apache successfully installed with OpenSSL and PHP"
+          echo "Check hydration_log for more details"
+
+      elif [ "$webserver" == "n" ]; then
+        if [ "$OS" == "centos" -a "redhat" ]; then
+          { # Installs base nginx stack
+          yum -y install nginx httpd-tools php php-common php-gd php-xmlrpc php-xml php-fpm openssl openssl-devel
+            # Starts nginx and php-fpm with Systemd or init script
+            if [ "$SYS" == "systemd" ]; then
+              systemctl start nginx && systemctl enable nginx
+              systemctl start php-fpm && systemctl enable php-fpm
+            elif [ "$SYS" == "sysvinit" ]; then
+              service nginx start && chkconfig nginx on
+              service php-fpm start && chkconfig nginx on
+            fi
+          fi } >> ~/hydration_log
+          echo "nginx successfully installed with OpenSSL and PHP-FPM"
+          echo "Check hydration_log for more details"
+      ;;
+    2)
+      echo "MySQL/MariaDB or PGSQL? [m/p]"
+      read dbserver
+        if [ "$dbserver" == "m" ]; then
+          if [ "$OS" == "centos" -a "redhat" ]; then
+          { # Installs MySQL server
+          yum -y install mysql mysql-server
+            # Starts MariaDB/MySQL with Systemd or init script
+            if [ "$SYS" == "systemd" ]; then
+              systemctl start mysql && systemctl enable mysql
+            elif [ "$SYS" == "sysvinit" ]; then
+              service mysql start && chkconfig mysql on
+            fi
+          fi } >> ~/hydration_log
+          echo "MySQL successfully installed"
+          echo "Check hydration_log for more details"
+
+          elif [ "$OS" == "ubuntu" ]; then
+            { # Installs MySQL server
+            apt-get -y install mysql mysql-server
+            # Starts MariaDB/MySQL with Ssytemd or init script
+            if [ "$SYS" == "systemd" ]; then
+              systemctl start mysql && systemctl enable mysql
+            elif [ "$SYS" == "sysvinit" ]; then
+              service mysql start && chkconfig mysql on
+            fi
+          fi } >> ~/hydration_log
+          echo "MySQL successfully installed"
+          echo "Check hydration_log for more details"
+
+        elif [ "$dbserver" == "p" ]; then
