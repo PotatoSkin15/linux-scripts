e58fd9df129217e95a6c486e03e1529dd6c57f63
Kenny <kenny@kenny-peck.com>
2016-06-16 13:28:39 -0400

    More support for Oracle Linux and Amazon EC2 AMI

--- flightcheck.sh
+++ flightcheck.sh
@@ -3,7 +3,7 @@
 clear
 
 # Check distribution before installing packages
-OS=`grep -Eiom 1 'CentOS|RedHat|Red Hat|Ubuntu|Fedora|SUSE' /proc/version`
+OS=`grep -Eiom 1 'CentOS|RedHat|euk|Ubuntu|Fedora|SUSE|amzn' /proc/version`
 
 # Check for Systemd vs sysvinit
 SYS=`ps -p 1 | grep -Eiom 1 'systemd|init'`
@@ -13,7 +13,7 @@ if [ "$USER" != "root" ]; then
 		echo 'WARNING! This script should be run as root'
 		echo 'Please enter sudo su and run the script again'
 else
-	if [ "$OS" == "centos" -a "redhat" -a 'Red Hat' -a "fedora" ]; then
+	if [ "$OS" == 'centos' -a 'redhat' -a 'euk' -a 'fedora' -a 'amzn' ]; then
 	echo 'Red Hat Derivative Detected'
 	echo 'Processing...'
 	{
@@ -38,7 +38,7 @@ else
 	} >> ~/flightcheck_log
 	echo 'Done. Check flightcheck_log for more details'
 
-	elif [ "$OS" == "ubuntu" ]; then
+	elif [ "$OS" == 'ubuntu' ]; then
 	echo 'Ubuntu Detected'
 	echo 'Processing...'
 	{
@@ -60,7 +60,7 @@ else
 	} >> ~/flightcheck_log
 	echo 'Done. Check flighcheck_log for more details'
 
-	elif [ "$OS" == "SUSE" ]; then
+	elif [ "$OS" == 'SUSE' ]; then
 	echo 'OpenSUSE Detected'
 	echo 'Processing...'
 	{
